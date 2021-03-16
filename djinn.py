5792a6c84c5d18806ee39aacccc0b2827ee4cb7e
Rafael Branco <rafael.p.branco@tecnico.ulisboa.pt>
2021-03-16 10:46:51 +0000

    Remove: unnecessary functions. Add: spam protection. Add: new movie list. Fix: wait time before counting the votes

--- djinn.py
+++ djinn.py
@@ -54,17 +54,10 @@ class Djinn(discord.Client):
         super().__init__(loop=loop, **options)
         self.movie_db = movie_db
         self.vote_emoji = b'\xf0\x9f\x91\x8d'.decode('utf-8')
+        self.channels_with_polls = set()
 
     async def on_ready(self) -> None:
         print(f'{self.user} is ready to start working!')
-
-    def integer_to_emoji(self, integer: int) -> str:
-        encoded_emoji = str(integer).encode(
-            'utf-8') + b'\xef\xb8\x8f\xe2\x83\xa3'
-        return encoded_emoji.decode('utf-8')
-
-    def emoji_to_integer(self, emoji: str) -> int:
-        return int(emoji[0])
 
     def format_movie_embed(self, movie_metadata: Dict) -> discord.Embed:
         title = movie_metadata.get('Title')
@@ -103,10 +96,11 @@ class Djinn(discord.Client):
             votes[reaction_count].append(message)
         return votes
 
-    async def start_movie_poll(self, channel: discord.abc.Messageable):
+    async def movie_poll(self, channel: discord.abc.Messageable):
+        self.channels_with_polls.add(channel)
         await channel.send('Wait while I search my boundless library')
 
-        movie_embeds: List[discord.Embed] = self.random_movie_embeds(3)
+        movie_embeds: List[discord.Embed] = self.random_movie_embeds(5)
 
         messages: List[discord.message.Message] = list()
         for embed in movie_embeds:
@@ -115,7 +109,7 @@ class Djinn(discord.Client):
             messages.append(message)
 
         await channel.send('I will wait 10 minutes before counting the votes.')
-        await asyncio.sleep(10)
+        await asyncio.sleep(600)
         await channel.send('I will start counting the votes.')
 
         election_results = await self.count_votes(channel, messages)
@@ -124,19 +118,26 @@ class Djinn(discord.Client):
         result = candidates[0] if len(candidates) == 1 else random.choice(
             candidates)
         await result.reply('You shall watch this movie.')
+        self.channels_with_polls.remove(channel)
 
     async def on_message(self, message: discord.message.Message) -> None:
         if message.author == self.user or self.user not in message.mentions:
             return
 
         if 'give me movies' in message.content.lower():
-            await self.start_movie_poll(message.channel)
+            if message.channel in self.channels_with_polls:
+                await message.channel.send('Stop spamming!')
+                return
+
+            await self.movie_poll(message.channel)
 
 
 if __name__ == '__main__':
     DISCORD_TOKEN = load('keys/discord_token')
     OMDB_API_KEY = load('keys/omdb_api_key')
-    movie_db = MovieDB('https://www.omdbapi.com/', apikey=OMDB_API_KEY)
+    movie_db = MovieDB('https://www.omdbapi.com/',
+                       imdb_ids_path='resources/1000_movies',
+                       apikey=OMDB_API_KEY)
 
     djinn = Djinn(movie_db)
     djinn.run(DISCORD_TOKEN)
