2f1b6b304f6aa3bf7273186eae6885c6b13325e9
Rafael Branco <rafael.p.branco@tecnico.ulisboa.pt>
2021-03-22 20:58:50 +0000

    Add: IMDB can get poster url from IMDB. Refactor: IMDB more flexible table creation and handling

--- imdb.py
+++ imdb.py
@@ -1,44 +1,124 @@
 #!/usr/bin/env python3
 
+import os
+import re
 import gzip
 import sqlite3
 import requests
 
-class IMDB():
-    def __init__(self, filename: str = 'resources/movies.db',) -> None:
-        self.dataset_urls = {
-            'title_basics': 'https://datasets.imdbws.com/title.basics.tsv.gz',
-            'title_ratings': 'https://datasets.imdbws.com/title.ratings.tsv.gz'
-        }
+from abc import (ABC, abstractmethod)
+from typing import (List, Optional)
+from dataclasses import dataclass
 
+@dataclass
+class Movie():
+    tconst: str
+    primary_title: str
+    original_title: str
+    is_adult: int
+    year: int
+    runtime: int
+    generes: str
+    rating: float
+    votes: int
+
+    def post_url(self) -> Optional[str]:
+        movie_url = f'https://www.imdb.com/title/{self.tconst}/'
+        response = requests.get(movie_url)
+        poster_url_pattern = r'^\s*"image":\s*"(https:\/\/.+\.jpg)",$'
+        html_content = response.content.decode('utf-8')
+        match = re.search(poster_url_pattern, html_content, flags=re.MULTILINE)
+        return match.group(1) if match else match
+
+@dataclass
+class Table(ABC):
+    cursor: sqlite3.Cursor
+    connection: sqlite3.Connection
+    name: str
+    dataset_url: str
+    schema: str
+
+    def download(self) -> Optional[bool]:
+        response = requests.get(self.dataset_url)
+        with requests.get(self.dataset_url, stream = True) as response_stream:
+            with open(f'{self.name}.tsv.gz', 'wb') as file:
+                for chunk in response_stream.iter_content(chunk_size=8192):
+                    file.write(chunk)
+        return True
+
+    def load_data_from_file(self) -> None:
+        with gzip.open(f'{self.name}.tsv.gz', 'r') as file:
+            skip = True
+            for line in file:
+                if skip: # skip header
+                    skip = False
+                    continue
+                yield line.decode('utf-8').strip().split('\t')
+
+    def drop(self) -> None:
+        self.cursor.execute(f'DROP TABLE IF EXISTS {self.name}')
+        self.connection.commit()
+
+    def create(self) -> None:
+        self.cursor.execute(f'CREATE TABLE {self.name} {self.schema}')
+        self.connection.commit()
+
+    def cleanup(self) -> None:
+        if os.path.exists(f'{self.name}.tsv.gz'):
+            os.remove(f'{self.name}.tsv.gz')
+
+    @abstractmethod
+    def insert(self) -> None:
+        pass
+
+class MoviesTable(Table):
+    def insert(self) -> None:
+        is_movie = lambda title: title[1] == 'movie'
+        for title in filter(is_movie, self.load_data_from_file()):
+            self.cursor.execute('INSERT INTO movies VALUES (?, ?, ?, ?, ?, ?, ?)', (title[0], title[2], title[3], title[4], title[5], title[7], title[8]))
+        self.connection.commit()
+
+class RatingsTable(Table):
+    def insert(self) -> None:
+        for title in self.load_data_from_file():
+            self.cursor.execute('INSERT INTO ratings VALUES (?, ?, ?)', (title[0], title[1], title[2]))
+        self.connection.commit()
+
+class IMDB():
+
+    def __init__(self, filename: str = 'resources/movies.db',) -> None:
         self.connection = sqlite3.connect(filename)
         self.cursor = self.connection.cursor()
+        self.tables = (
+            MoviesTable(
+                cursor = self.cursor,
+                connection = self.connection,
+                name = 'movies',
+                dataset_url = 'https://datasets.imdbws.com/title.basics.tsv.gz',
+                schema = '(tconst TEXT, primary_title TEXT, original_title TEXT, is_adult INTEGER, year INTEGER, runtime INTEGER, genres TEXT)',
+            ),
+            RatingsTable(
+                cursor = self.cursor,
+                connection = self.connection,
+                name = 'ratings',
+                dataset_url = 'https://datasets.imdbws.com/title.ratings.tsv.gz',
+                schema = '(tconst TEXT, rating REAL, votes INTEGER)',
+            ),
+        )
 
-    def download_dataset(self, filename: str, url: str) -> None:
-        response = requests.get(url)
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+    def update(self) -> None:
+        for table in self.tables:
+            if table.download():
+                table.drop()
+                table.create()
+                table.insert()
+            table.cleanup()
 
-    def update(self) -> bool:
-        self.cursor.execute('DROP TABLE IF EXISTS movies')
-        self.cursor.execute('DROP TABLE IF EXISTS ratings')
-        self.connection.commit()
-
-        for name, url in self.dataset_urls.items():
-            # self.download_dataset(f'{name}.tsv.gz', url)
-            pass
-
-        self.cursor.execute('CREATE TABLE movies (tconst TEXT, primaryTitle TEXT, originalTitle TEXT, isAdult INTEGER, year INTEGER, runtime INTEGER, genres TEXT)')
-        self.cursor.execute('CREATE TABLE ratings (tconst TEXT, rating REAL, votes INTEGER)')
-        self.connection.commit()
-
-        with gzip.open('title_basics.tsv.gz', 'r') as file:
-            for line in file:
-                values = line.decode('utf-8').split('\t')
-                if values[1] == 'movie':
-                    self.cursor.execute('INSERT INTO movies VALUES (?, ?, ?, ?, ?, ?, ?)', (values[0], values[2], values[3], values[4], values[5], values[7], values[8]))
-            self.connection.commit()
+    def random_movies(self, ratings: int = 1000, number: int = 3) -> List:
+        self.cursor.execute('SELECT * FROM movies NATURAL JOIN ratings WHERE votes > ? ORDER BY RANDOM() limit ?', (ratings, number))
+        print(tuple(map(lambda movie: Movie(*movie).post_url(), self.cursor.fetchall())))
 
 if __name__ == '__main__':
     imdb = IMDB()
     imdb.update()
+    imdb.random_movies()
