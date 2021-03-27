c112d3652172fc9ae5531f6ab4c16db1899994ea
Rafael Branco <rafael.p.branco@tecnico.ulisboa.pt>
2021-03-27 00:11:05 +0000

    Refactor: command logic and bot inner workings

--- djinn.py
+++ djinn.py
@@ -9,6 +9,7 @@ import requests
 
 from imdb import (IMDB, Movie)
 from collections import defaultdict
+from abc import (ABC, abstractmethod)
 from typing import (Dict, List, Union, Any, Optional, Tuple, Union, Callable)
 
 
@@ -16,26 +17,26 @@ def load(path: str) -> str:
     with open(path, 'r') as f:
         return f.read().strip()
 
-class Query():
 
+class Query():
     @staticmethod
     def parse_limit(
-        parameter_name: str,
-        raw_query: str,
-        limit_type: Callable = int,
-        default: Tuple[str, int] = ('>', 0),
+            parameter_name: str,
+            raw_query: str,
+            limit_type: Callable = int,
+            default: Tuple[str, int] = ('>', 0),
     ) -> Tuple[str, Any]:
         pattern = f'\(.*{parameter_name} *([=<>]) *(\d+(.\d+)?).*\)'
         match = re.search(pattern, raw_query)
         if match:
-            return (match.group(1), amount_type(match.group(2)))
+            return (match.group(1), limit_type(match.group(2)))
         return default
 
     @staticmethod
     def parse_genre(raw_query: str, default: str = '') -> str:
         match = re.search(r'\(.*genre *= *(\w+).*\)', raw_query)
         if match:
-            return genre_match.group(1)
+            return match.group(1)
         return default
 
     @staticmethod
@@ -45,7 +46,7 @@ class Query():
             return int(match.group(2))
         return default
 
-    def __init__(self, raw_query: str = ''):
+    def __init__(self, raw_query: str = '') -> None:
         self.amount = Query.parse_amount(raw_query)
         self.rating = Query.parse_limit('rating', raw_query, float)
         self.votes = Query.parse_limit('votes', raw_query, int)
@@ -56,8 +57,7 @@ class Query():
         return self.__dict__.copy()
 
 
-class Command():
-
+class Command(ABC):
     @staticmethod
     def parse_command_identifier(message: str) -> str:
         # TODO: use class identifier to update pattern
@@ -68,73 +68,125 @@ class Command():
         return match
 
     @classmethod
-    def build(cls, message: str) -> Optional['Command']:
+    def build(
+        cls,
+        bot: discord.Client,
+        channel: discord.abc.Messageable,
+        message: str,
+    ) -> Optional['Command']:
         identifier = Command.parse_command_identifier(message)
         for subclass in cls.__subclasses__():
             if identifier == subclass.identifier:
-                return subclass(
-                    Query(message),
-                )
+                return subclass(bot, channel, Query(message))
         return None
 
     def __init__(
-        self,
-        query: Query = Query(),
+            self,
+            bot: discord.Client,
+            channel: discord.abc.Messageable,
+            query: Query = Query(),
     ) -> None:
+        self.bot = bot
+        self.channel = channel
         self.query = query
+
+    @abstractmethod
+    async def process(self):
+        pass
+
+    @staticmethod
+    def format_movie_embed(movie: Movie) -> discord.Embed:
+        poster_url = movie.poster_url()
+        embed = discord.Embed(title=f'{movie.original_title} ({movie.year})',
+                              description=movie.url,
+                              color=0xe2b616)
+        if poster_url not in (None, 'n/a', 'N/A'):
+            embed.set_image(url=poster_url)
+
+        embed.add_field(name='Rating', value=f'{movie.rating}/10')
+        embed.add_field(name='Votes', value=f'{movie.votes}')
+        embed.add_field(name='Duration', value=f'{movie.runtime} minutes')
+        embed.add_field(name='Genres',
+                        value=f'{movie.genres.replace(",", ", ")}')
+        return embed
+
+    def random_movie_embeds(self, ) -> List[discord.Embed]:
+        movie_embeds: List[discord.Embed] = list()
+        for movie in self.bot.movie_db.random_movies(**self.query.to_dict()):
+            embed = Command.format_movie_embed(movie)
+            movie_embeds.append(embed)
+        return movie_embeds
+
+    async def publish_movies(
+        self,
+        reaction: str = None,
+    ) -> List[discord.message.Message]:
+        movie_embeds: List[discord.Embed] = self.random_movie_embeds()
+
+        messages: List[discord.message.Message] = list()
+        for embed in movie_embeds:
+            message = await self.channel.send(embed=embed)
+            if reaction:
+                await message.add_reaction(reaction)
+            messages.append(message)
+        return messages
 
 
 class Fetch(Command):
     identifier: str = 'fetch'
 
+    async def process(self, bot, channel):
+        if not self.bot.register_channel(self.channel):
+            return await self.channel.send('Stop spamming.')
 
-class Request():
-    operation: Tuple[str, int] = None
-    rating: Tuple[str, float] = None
-    votes: Tuple[str, int] = None
-    duration: Tuple[str, int] = None
-    genre: str = None
+        await self.publish_movies()
 
-    @staticmethod
-    def parse_parameter(
-        parameter_name: str,
-        message: str,
-        amount_type: Callable = int,
-    ) -> Tuple[str, Union[int, float]]:
-        match = re.search(f'\(.*{parameter_name} *([=<>]) *(\d+(.\d+)?).*\)',
-                          message)
-        if match:
-            return (match.group(1), amount_type(match.group(2)))
-        return match
+        self.bot.delist_channel(self.channel)
 
-    def __init__(self, message: str) -> None:
-        operation_match = re.search(r'(fetch|poll)( \d)', message)
-        if not operation_match:
-            return
-        self.operation = (operation_match.group(1),
-                          int(operation_match.group(2)))
 
-        self.rating = Request.parse_parameter('rating', message, float)
-        self.votes = Request.parse_parameter('votes', message)
-        self.duration = Request.parse_parameter('duration', message)
+class Poll(Command):
+    identifier: str = 'poll'
 
-        genre_match = re.search(r'\(.*genre *= *(\w+).*\)', message)
-        if genre_match:
-            self.genre = genre_match.group(1)
+    async def count_votes(
+        self,
+        messages: List[discord.message.Message],
+    ) -> Dict[int, List[discord.message.Message]]:
+        votes = defaultdict(list)
+        for index, message in enumerate(messages):
+            message = await self.channel.fetch_message(message.id)
+            valid_reactions = filter(
+                lambda reaction: reaction.emoji == Djinn.vote_emoji,
+                message.reactions)
+            reaction_count = next(valid_reactions).count
+            votes[reaction_count].append(message)
+        return votes
 
-    def query(self) -> Dict:
-        arguments = dict()
-        if self.operation:
-            arguments['amount'] = self.operation[1]
-        if self.rating:
-            arguments['rating'] = self.rating
-        if self.votes:
-            arguments['votes'] = self.votes
-        if self.duration:
-            arguments['duration'] = self.duration
-        if self.genre:
-            arguments['genre'] = self.genre
-        return arguments
+    async def broadcast_winner(
+        self,
+        election_results: Dict[int, List[discord.message.Message]],
+    ) -> None:
+        candidates = election_results[max(election_results)]
+        result = random.choice(candidates)
+        await result.reply('You shall watch this movie.')
+
+    async def wait_to_count_votes(self, minutes: int) -> None:
+        await self.channel.send(
+            f'I will wait {minutes} minutes before counting the votes.')
+        await asyncio.sleep(minutes * 60)
+        await self.channel.send('I will start counting the votes.')
+
+    async def process(self, bot, channel):
+        if not self.bot.register_channel(self.channel):
+            return await self.channel.send('Stop spamming.')
+
+        await self.channel.send('Wait while I search my boundless library')
+
+        messages = await self.publish_movies(reaction=bot.vote_emoji)
+        await self.wait_to_count_votes(10)
+        election_results = await self.count_votes(messages)
+        await self.broadcast_winner(election_results)
+
+        self.bot.delist_channel(self.channel)
 
 
 class Djinn(discord.Client):
@@ -150,108 +202,34 @@ class Djinn(discord.Client):
     ) -> None:
         super().__init__(loop=loop, **options)
         self.movie_db = movie_db
-        self.channels_with_polls = set()
+        self.busy_channels = set()
 
     async def on_ready(self) -> None:
         print(f'{self.user} is ready to start working!')
 
-    def format_movie_embed(self, movie: Movie) -> discord.Embed:
-        poster_url = movie.poster_url()
-        embed = discord.Embed(title=f'{movie.original_title} ({movie.year})',
-                              description=movie.url,
-                              color=0xe2b616)
-        if poster_url not in (None, 'n/a', 'N/A'):
-            embed.set_image(url=poster_url)
+    def register_channel(self, channel: discord.abc.Messageable) -> bool:
+        if channel not in self.busy_channels:
+            self.busy_channels.add(channel)
+            return True
+        return False
 
-        embed.add_field(name='Rating', value=f'{movie.rating}/10')
-        embed.add_field(name='Votes', value=f'{movie.votes}')
-        embed.add_field(name='Duration', value=f'{movie.runtime} minutes')
-        embed.add_field(name='Genres',
-                        value=f'{movie.genres.replace(",", ", ")}')
-        return embed
-
-    def random_movie_embeds(
-        self,
-        **options: Any,
-    ) -> List[discord.Embed]:
-        movie_embeds: List[discord.Embed] = list()
-        for movie in self.movie_db.random_movies(**options):
-            embed = self.format_movie_embed(movie)
-            movie_embeds.append(embed)
-        return movie_embeds
-
-    async def count_votes(
-        self,
-        channel: discord.abc.Messageable,
-        messages: List[discord.message.Message],
-    ) -> Dict[int, List[discord.message.Message]]:
-        votes = defaultdict(list)
-        for index, message in enumerate(messages):
-            message = await channel.fetch_message(message.id)
-            valid_reactions = filter(
-                lambda reaction: reaction.emoji == Djinn.vote_emoji,
-                message.reactions)
-            reaction_count = next(valid_reactions).count
-            votes[reaction_count].append(message)
-        return votes
-
-    async def wait_to_count_votes(
-        minutes: int,
-        channel: discord.abc.Messageable,
-    ) -> None:
-        await channel.send(
-            f'I will wait {minutes} minutes before counting the votes.')
-        await asyncio.sleep(minutes * 60)
-        await channel.send('I will start counting the votes.')
-
-    async def poll(self,
-                   channel: discord.abc.Messageable,
-                   options: Dict = {}) -> None:
-        self.channels_with_polls.add(channel)
-        await channel.send('Wait while I search my boundless library')
-
-        messages = await self.publish_movies(channel=channel,
-                                             options=options,
-                                             reaction=Djinn.vote_emoji)
-
-        await self.wait_to_count_votes(10)
-
-        election_results = await self.count_votes(channel, messages)
-
-        candidates = election_results[max(election_results)]
-        result = candidates[0] if len(candidates) == 1 else random.choice(
-            candidates)
-        await result.reply('You shall watch this movie.')
-        self.channels_with_polls.remove(channel)
-
-    async def publish_movies(
-            self,
-            channel: discord.abc.Messageable,
-            options: Dict = {},
-            reaction: str = None) -> List[discord.message.Message]:
-        movie_embeds: List[discord.Embed] = self.random_movie_embeds(**options)
-
-        messages: List[discord.message.Message] = list()
-        for embed in movie_embeds:
-            message = await channel.send(embed=embed)
-            if reaction is not None:
-                await message.add_reaction(reaction)
-            messages.append(message)
-        return messages
+    def delist_channel(self, channel: discord.abc.Messageable) -> bool:
+        if channel in self.busy_channels:
+            self.busy_channels.remove(channel)
+            return True
+        return False
 
     async def on_message(self, message: discord.message.Message) -> None:
         if message.author == self.user or self.user not in message.mentions:
             return
 
-        request = Request(message.content.lower())
-        if request.operation:
-            if message.channel in self.channels_with_polls:
-                await message.channel.send('Stop spamming!')
-            if request.operation[0] == 'fetch':
-                await self.publish_movies(channel=message.channel,
-                                          options=request.query())
-            elif request.operation[0] == 'poll':
-                await self.poll(message.channel, options=request.query())
+        command = Command.build(
+            bot=self,
+            channel=message.channel,
+            message=message.content.lower(),
+        )
+        if command:
+            await command.process(bot=self, channel=message.channel)
 
 
 if __name__ == '__main__':
